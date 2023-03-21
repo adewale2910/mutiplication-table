be74968bf367d4f77b7a8a08496414df1698c3a2
Adeloye Olusegun Adewale <adeloyeolusegun58@gmail.com>
2023-03-21 11:33:35 +0100

    multiplication table using loop

---
+++ loop.js
@@ -0,0 +1,15 @@
+var multiply = input.value
+function check() {
+    for(i = 1; i <=12; i ++){
+        for(k = 1; k<=12; i++){
+            console
+        }
+    }
+    myTable.innerHTML = myTable.innerHTML +
+            `<tr>
+        <td>2*${i} = ${2*i}</td>
+    </tr>`
+    
+}
+
+
